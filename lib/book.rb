1b7a02e8083dde5128abcb3814ef2c9066707386
Philip Levy <philipdlevy@gmail.com>
2022-05-25 10:13:56 -0600

    commit tree 4d75ddc47af8eb42c3f53f745b12541d961c8bcc

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,33 @@
+# class Book 
+#     attr_reader :title, :author_name, :page_count, :genre
 
+#     def initialize(title)
+#         @title = title
+#         @author_name = author_name
+#         @page_count = page_count
+#         @genre = genre
+#     end
+
+#     def book
+#         "#{title} #{author_name} #{page_count} #{genre}".strip
+#     end
+# end
+
+class Book 
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+   
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("Some Title")
+book.turn_page
 
